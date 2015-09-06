bbfcdb9f6affa81fa0e1cd158df1ce16f848965b
Sean Rhone <espionage724@tutanota.com>
2015-09-06 13:01:19 -0400

    Create update-oak.sh
    Script used to update software present on Oak

---
+++ update-oak.sh
@@ -0,0 +1,12 @@
+cd '/home/espionage724/mangos'
+git pull
+cd '/home/espionage724/mangos/src/bindings/ScriptDev2'
+git pull
+cd '/home/espionage724/acid'
+git pull
+cd '/home/espionage724/classicdb'
+git pull
+sudo apt-get update
+sudo apt-get dist-upgrade
+sync
+cd '/home/espionage724'
