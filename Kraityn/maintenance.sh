36b37a26b21880f3b4f68f4494087dc0d52f9f71
Sean Rhone <espionage724@tutanota.com>
2015-09-07 07:45:26 -0400

    Update maintenance.sh
    - Added sync and home directory cd

--- Kraityn/maintenance.sh
+++ Kraityn/maintenance.sh
@@ -3,3 +3,9 @@ mysqlcheck -u root -p --auto-repair --optimize --all-databases
 
 # Clear caches (possibly)
 sudo service apache2 graceful
+
+# Write changes to disk
+sync
+
+# Finish up
+cd '/home/espionage724/'
