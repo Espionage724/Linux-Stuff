fa929573720fe2f21b441d647ef128ca74c74b81
Sean Rhone <espionage724@tutanota.com>
2015-09-07 07:36:03 -0400

    Create update-wow.sh
    - Gave CMaNGOS its own script; no need to update it along with the distro if it wasn't being compiled anyway

---
+++ Oak/update-wow.sh
@@ -0,0 +1,25 @@
+# Update CMaNGOS
+cd '/home/espionage724/mangos'
+git pull
+
+# Update ScriptDev2
+cd '/home/espionage724/mangos/src/bindings/ScriptDev2'
+git pull
+
+# Update ACID
+cd '/home/espionage724/acid'
+git pull
+
+# Update ClassicDB
+cd '/home/espionage724/classicdb'
+git pull
+
+# Recompile CMaNGOS and ScriptDev2
+cd '/home/espionage724/build'
+make clean
+cmake '/home/espionage724/mangos' -DCMAKE_INSTALL_PREFIX='/home/espionage724/run' -DINCLUDE_BINDINGS_DIR=ScriptDev2 -DPCH=1 -DDEBUG=0 -DWARNINGS=0
+make -j2
+make install
+
+# Resync ClassicDB
+cd '/home/espionage724/classicdb'
