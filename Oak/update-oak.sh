b155676ac9321e612cf66b6146194a80e445474a
Sean Rhone <espionage724@tutanota.com>
2015-09-07 07:36:58 -0400

    Update update-oak.sh
    - Removed CMaNGOS git pulls and put them into separate script; no need to resync repos if server updating isn't planned

--- Oak/update-oak.sh
+++ Oak/update-oak.sh
@@ -1,19 +1,3 @@
-# Update CMaNGOS
-cd '/home/espionage724/mangos'
-git pull
-
-# Update ScriptDev2
-cd '/home/espionage724/mangos/src/bindings/ScriptDev2'
-git pull
-
-# Update ACID
-cd '/home/espionage724/acid'
-git pull
-
-# Update ClassicDB
-cd '/home/espionage724/classicdb'
-git pull
-
 # Update distro
 sudo apt-get update
 sudo apt-get dist-upgrade
