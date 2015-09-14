877bdf68db91448c11adcf833ad139235fca4da7
Sean Rhone <espionage724@tutanota.com>
2015-09-13 21:03:55 -0400

    Update update-wow.sh
    - Fixed script

--- Oak/update-wow.sh
+++ Oak/update-wow.sh
@@ -21,7 +21,7 @@ sync
 cd '/home/espionage724/build'
 make clean
 cmake '/home/espionage724/mangos' -DCMAKE_INSTALL_PREFIX='/home/espionage724/run' -DINCLUDE_BINDINGS_DIR=ScriptDev2 -DPCH=1 -DDEBUG=0 -DWARNINGS=0
-make -j2
+make -j4
 make install
 
 # Write changes to disk
@@ -29,6 +29,13 @@ sync
 
 # Resync ClassicDB
 cd '/home/espionage724/classicdb'
+./InstallFullDB.sh
+
+# Database SQL Updates
+# Change this if mangosd fails to start because of some update or if ClassicDB fixes something
+mysql -u mangos -h 192.168.1.153 -p mangos < '/home/espionage724/mangos/sql/updates/0.12.3/z2678_03_mangos.sql'
+mysql -u mangos -h 192.168.1.153 -p mangos < '/home/espionage724/mangos/sql/updates/0.12.3/z2680_01_mangos_battleground_template.sql'
+mysql -u mangos -h 192.168.1.153 -p mangos < '/home/espionage724/mangos/sql/updates/0.12.3/z2681_01_mangos_mangos_string.sql'
 
 # Write changes to disk
 sync
