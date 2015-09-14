1c3606050396c8b3b4875e7f3e73151ba4094d5c
Sean Rhone <espionage724@tutanota.com>
2015-09-14 15:06:47 -0400

    Update update-wow.sh
    - Added another pause
    - Added make clean
    - Fixed script for UDB

--- Oak/update-wow.sh
+++ Oak/update-wow.sh
@@ -14,16 +14,20 @@ git pull
 cd '/home/espionage724/acid'
 git pull
 
-# Update ClassicDB
-cd '/home/espionage724/classicdb'
+# Update UDB
+cd '/home/espionage724/udb'
 git pull
 
 # Write changes to disk
 sync
 
+# Verify
+echo "If all 4 options above are already up-to-date, no point in continuing; press Ctrl + C to stop in 5 seconds"
+sleep 5
+
 # Recompile CMaNGOS and ScriptDev2
 cd '/home/espionage724/build'
-
+make clean
 cmake '/home/espionage724/mangos' -DCMAKE_INSTALL_PREFIX='/home/espionage724/run' -DINCLUDE_BINDINGS_DIR=ScriptDev2 -DPCH=1 -DDEBUG=0 -DWARNINGS=0
 make -j4
 make install
@@ -31,15 +35,9 @@ make install
 # Write changes to disk
 sync
 
-# Resync ClassicDB
-cd '/home/espionage724/classicdb'
+# Resync UDB
+cd '/home/espionage724/udb'
 ./InstallFullDB.sh
-
-# Database SQL Updates
-# Change this if mangosd fails to start because of some update or if ClassicDB fixes something
-mysql -u mangos -h 192.168.1.153 -p < '/home/espionage724/mangos/sql/updates/0.12.3/z2678_03_mangos.sql'
-mysql -u mangos -h 192.168.1.153 -p < '/home/espionage724/mangos/sql/updates/0.12.3/z2680_01_mangos_battleground_template.sql'
-mysql -u mangos -h 192.168.1.153 -p < '/home/espionage724/mangos/sql/updates/0.12.3/z2681_01_mangos_mangos_string.sql'
 
 # Write changes to disk
 sync
