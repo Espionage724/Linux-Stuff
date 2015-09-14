daddbf53a9cd95c5037c6af84550787ca3ecfcd3
Sean Rhone <espionage724@tutanota.com>
2015-09-13 21:24:48 -0400

    Update update-wow.sh

--- Oak/update-wow.sh
+++ Oak/update-wow.sh
@@ -1,3 +1,7 @@
+# Warning
+echo "MAKE SURE THAT REALMD AND MANGOSD ARE STOPPED, you have 15 seconds to cancel the script"
+sleep 15
+
 # Update CMaNGOS
 cd '/home/espionage724/mangos'
 git pull
@@ -19,7 +23,7 @@ sync
 
 # Recompile CMaNGOS and ScriptDev2
 cd '/home/espionage724/build'
-make clean
+
 cmake '/home/espionage724/mangos' -DCMAKE_INSTALL_PREFIX='/home/espionage724/run' -DINCLUDE_BINDINGS_DIR=ScriptDev2 -DPCH=1 -DDEBUG=0 -DWARNINGS=0
 make -j4
 make install
@@ -33,9 +37,9 @@ cd '/home/espionage724/classicdb'
 
 # Database SQL Updates
 # Change this if mangosd fails to start because of some update or if ClassicDB fixes something
-mysql -u mangos -h 192.168.1.153 -p mangos < '/home/espionage724/mangos/sql/updates/0.12.3/z2678_03_mangos.sql'
-mysql -u mangos -h 192.168.1.153 -p mangos < '/home/espionage724/mangos/sql/updates/0.12.3/z2680_01_mangos_battleground_template.sql'
-mysql -u mangos -h 192.168.1.153 -p mangos < '/home/espionage724/mangos/sql/updates/0.12.3/z2681_01_mangos_mangos_string.sql'
+mysql -u mangos -h 192.168.1.153 --password=CNG mangos < '/home/espionage724/mangos/sql/updates/0.12.3/z2678_03_mangos.sql'
+mysql -u mangos -h 192.168.1.153 --password=CNG mangos < '/home/espionage724/mangos/sql/updates/0.12.3/z2680_01_mangos_battleground_template.sql'
+mysql -u mangos -h 192.168.1.153 --password=CNG mangos < '/home/espionage724/mangos/sql/updates/0.12.3/z2681_01_mangos_mangos_string.sql'
 
 # Write changes to disk
 sync
