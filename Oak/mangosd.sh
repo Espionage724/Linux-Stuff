d0124c22e28c87d98c392a936097954ec2ff85e7
Sean Rhone <espionage724@tutanota.com>
2015-09-16 21:56:59 -0400

    Update and rename mangosd.sh to worldserver.sh
    - Updated for TrinityCore

--- Oak/mangosd.sh
+++
@@ -1,2 +0,0 @@
-cd '/home/espionage724/run/bin'
-screen -dmS mangosd '/home/espionage724/run/bin/mangosd' -c '/home/espionage724/run/etc/mangosd.conf' -a '/home/espionage724/run/etc/ahbot.conf'
---
+++ Oak/worldserver.sh
@@ -0,0 +1,2 @@
+cd '/home/espionage724/run/bin'
+screen -dmS worldserver '/home/espionage724/run/bin/worldserver'
