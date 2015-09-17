c6670b532a69683c79c07b83ab04986e8a625900
Sean Rhone <espionage724@tutanota.com>
2015-09-16 21:58:00 -0400

    Create systemctl.sh

---
+++ Oak/systemctl.sh
@@ -0,0 +1,6 @@
+sudo systemctl disable minetest
+sudo systemctl disable bnetserver
+sudo systemctl disable worldserver
+sudo systemctl enable minetest
+sudo systemctl enable bnetserver
+sudo systemctl enable worldserver
