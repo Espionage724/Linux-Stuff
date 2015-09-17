5830f516cef3e3105b3077e97220a7b8517fbee3
Sean Rhone <espionage724@tutanota.com>
2015-09-16 21:56:27 -0400

    Update and rename realmd.sh to bnetserver.sh
    - Updated for TrinityCore

---
+++ Oak/bnetserver.sh
@@ -0,0 +1,2 @@
+cd '/home/espionage724/run/bin'
+screen -dmS bnetserver '/home/espionage724/run/bin/bnetserver'
--- Oak/realmd.sh
+++
@@ -1,2 +0,0 @@
-cd '/home/espionage724/run/bin'
-screen -dmS realmd '/home/espionage724/run/bin/realmd' -c '/home/espionage724/run/etc/realmd.conf'
