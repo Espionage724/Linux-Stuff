73b4a71515db41ea8b62cec6fef9a3571fb22bca
Sean Rhone <espionage724@tutanota.com>
2015-10-02 03:01:26 -0400

    Update b.sh
    - Change root to dedicated backup user

--- Kraityn/b.sh
+++ Kraityn/b.sh
@@ -1,10 +1,10 @@
 # Backup Databases
-mysqldump -u root -pX auth > '/home/espionage724/dumps/auth.sql'
-mysqldump -u root -pX characters > '/home/espionage724/dumps/characters.sql'
-mysqldump -u root -pX gnusocial > '/home/espionage724/dumps/gnusocial.sql'
-mysqldump -u root -pX joomla > '/home/espionage724/dumps/joomla.sql'
-mysqldump -u root -pX mybb > '/home/espionage724/dumps/mybb.sql'
-mysqldump -u root -pX wordpress > '/home/espionage724/dumps/wordpress.sql'
+mysqldump -u dump -pX auth > '/home/espionage724/dumps/auth.sql'
+mysqldump -u dump -pX characters > '/home/espionage724/dumps/characters.sql'
+mysqldump -u dump -pX gnusocial > '/home/espionage724/dumps/gnusocial.sql'
+mysqldump -u dump -pX joomla > '/home/espionage724/dumps/joomla.sql'
+mysqldump -u dump -pX mybb > '/home/espionage724/dumps/mybb.sql'
+mysqldump -u dump -pX wordpress > '/home/espionage724/dumps/wordpress.sql'
 
 # Archive Databases
 zip -r '/home/espionage724/backups/'backup-$(date +%Y-%m-%d).zip '/home/espionage724/dumps/'*.sql
