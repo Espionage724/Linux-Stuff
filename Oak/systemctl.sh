4b2b74a4bf2a17354d1f22e1b14d8f2f5a529ad1
Sean Rhone <espionage724@tutanota.com>
2015-10-17 03:13:47 -0400

    Delete systemctl.sh
    - Useless

--- Oak/systemctl.sh
+++
@@ -1,7 +0,0 @@
-sudo systemctl disable auth
-sudo systemctl disable world
-sudo systemctl disable mt
-sudo systemctl daemon-reload
-sudo systemctl enable auth
-sudo systemctl enable world
-sudo systemctl enable mt
