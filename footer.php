8f1db646b888c6b654421359881343d0adffe1ff
Sean Rhone <espionage724@tutanota.com>
2015-10-18 02:34:53 -0400

    Update footer.php
    - This is the footer used for the Joomla page at Realm of Espionage
    - Better wording used

--- footer.php
+++ footer.php
@@ -7,13 +7,13 @@
             This site is powered by <a href="https://www.joomla.org" title="Joomla" target="_blank">Joomla</a> and the responsive <a href="http://www.t3-framework.org/" title="T3" target="_blank">T3 Framework</a>. The template in-use is <a href="https://www.joomlart.com/joomla/templates/purity-iii" title="Purity III" target="_blank">Purity III</a>.
           </small>
           <small>
-            The content of <a href="https://www.realmofespionage.com" title="Realm of Espionage">Realm of Espionage</a> is licensed under the <a href="https://creativecommons.org/licenses/by-sa/4.0" title="CC BY-SA 4.0" target="_blank">Creative Commons Attribution-ShareAlike 4.0 International license</a>.
+            The content of <a href="https://www.realmofespionage.com" title="Realm of Espionage">Realm of Espionage</a> is licensed under the <a href="https://creativecommons.org/licenses/by-sa/4.0" title="CC BY-SA 4.0" target="_blank">Creative Commons Attribution-ShareAlike 4.0 International</a> license.
           </small>
           <small>
-            <a href="http://twitter.github.io/bootstrap/" title="Bootstrap by Twitter" target="_blank">Bootstrap</a> is a front-end framework of Twitter, Inc. Code licensed under <a href="https://github.com/twbs/bootstrap/blob/master/LICENSE" title="MIT License" target="_blank">MIT License</a>.
+            <a href="http://twitter.github.io/bootstrap/" title="Bootstrap by Twitter" target="_blank">Bootstrap</a> is a front-end framework of Twitter, Inc. Code is licensed under the <a href="https://github.com/twbs/bootstrap/blob/master/LICENSE" title="MIT License" target="_blank">MIT License</a>.
           </small>
           <small>
-            <a href="http://fortawesome.github.io/Font-Awesome/" target="_blank">Font Awesome</a> font licensed under <a href="http://scripts.sil.org/OFL">SIL OFL 1.1</a>.
+            <a href="http://fortawesome.github.io/Font-Awesome/" target="_blank">Font Awesome</a> font is licensed under <a href="http://scripts.sil.org/OFL">SIL OFL 1.1</a>.
           </small>
               </div>
 				<?php if ($this->getParam('t3-rmvlogo', 1)): ?>
