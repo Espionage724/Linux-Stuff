e08327c804e63300ff768d7266d80da9363fb809
Sean Rhone <espionage724@tutanota.com>
2015-10-19 03:58:10 -0400

    Create in.sh
    - This finishes installing TC and PvPGN

---
+++ Oak/in.sh
@@ -0,0 +1,20 @@
+# Stop services
+sudo systemctl stop auth world pvpgn
+
+# Install TrinityCore
+cd '/home/espionage724/build'
+make install
+
+# Install PvPGN
+cd '/home/espionage724/build-pvpgn'
+sudo make install
+
+# Take Ownership of PvPGN
+sudo chown -R espionage724:espionage724 '/home/espionage724/run-pvpgn'
+
+# Restart services
+sudo systemctl start auth world pvpgn
+
+# Finish up
+cd '/home/espionage724'
+sync
