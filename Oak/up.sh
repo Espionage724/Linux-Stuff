bf4130a36442a936fdc3ed1a2802e07352bb25bf
Sean Rhone <espionage724@tutanota.com>
2015-10-19 03:53:43 -0400

    Update up.sh
    - Added PvPGN

--- Oak/up.sh
+++ Oak/up.sh
@@ -5,6 +5,10 @@ sudo dnf update
 cd '/home/espionage724/trinitycore'
 git pull origin 3.3.5
 
+# Update PvPGN
+cd '/home/espionage724/pvpgn'
+git pull origin master
+
 # Write changes to disk
 sync
 
@@ -12,8 +16,14 @@ sync
 rm -R '/home/espionage724/build'
 mkdir '/home/espionage724/build'
 cd '/home/espionage724/build'
-CC='distcc gcc' CXX='distcc g++' cmake '/home/espionage724/trinitycore' -DTOOLS=1 -DCMAKE_INSTALL_PREFIX='/home/espionage724/run' -DWITH_WARNINGS=0 -DWITH_COREDEBUG=0 -DUSE_COREPCH=0 -DUSE_SCRIPTPCH=0 -DCMAKE_CXX_FLAGS='-O2 -pipe -march=amdfam10' -DCMAKE_C_FLAGS='-O2 -pipe -march=amdfam10'
-DISTCC_HOSTS='192.168.1.150/8 localhost/1' make -j9
+CC='distcc clang' CXX='distcc clang++' cmake '/home/espionage724/trinitycore' -DTOOLS=1 -DCMAKE_INSTALL_PREFIX='/home/espionage724/run' -DWITH_WARNINGS=0 -DWITH_COREDEBUG=0 -DUSE_COREPCH=0 -DUSE_SCRIPTPCH=0 -DCMAKE_CXX_FLAGS='-O2 -pipe -march=amdfam10' -DCMAKE_C_FLAGS='-O2 -pipe -march=amdfam10' && sync
+DISTCC_HOSTS='192.168.1.150/8 localhost/1' make -j9 && sync
+
+# Recompile PvPGN
+rm -R '/home/espionage724/build-pvpgn'
+mkdir '/home/espionage724/build-pvpgn'
+CC='distcc clang' CXX='distcc clang++' cmake '/home/espionage724/pvpgn' -DCMAKE_INSTALL_PREFIX='/home/espionage724/run-pvpgn' -DWITH_MYSQL=1 -DWITH_LUA=0 -DWITH_WIN32_GUI=0 -DCMAKE_CXX_FLAGS='-O2 -pipe -march=amdfam10' -DCMAKE_C_FLAGS='-O2 -pipe -march=amdfam10' && sync
+DISTCC_HOSTS='192.168.1.150/8 localhost/1' make -j9 && sync
 
 # Write changes to disk
 sync
