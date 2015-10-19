2791aa65b1d2d5da081eed6a4e8aa6e8cdd09990
Sean Rhone <espionage724@tutanota.com>
2015-10-19 05:46:15 -0400

    Update b.sh
    - Added PvPGN to backup script

--- Kraityn/b.sh
+++ Kraityn/b.sh
@@ -5,6 +5,7 @@ mysqldump -u dump -pX gnusocial > '/home/espionage724/dumps/gnusocial.sql'
 mysqldump -u dump -pX joomla > '/home/espionage724/dumps/joomla.sql'
 mysqldump -u dump -pX mybb > '/home/espionage724/dumps/mybb.sql'
 mysqldump -u dump -pX wordpress > '/home/espionage724/dumps/wordpress.sql'
+mysqldump -u dump -pX pvpgn > '/home/espionage724/dumps/pvpgn.sql'
 
 # Archive Databases
 zip -r '/home/espionage724/backups/'backup-$(date +%Y-%m-%d).zip '/home/espionage724/dumps/'*.sql
