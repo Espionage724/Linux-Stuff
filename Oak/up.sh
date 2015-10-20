b64a2f11223cb93322f05ee334ec79a5a8cb552d
Sean Rhone <espionage724@tutanota.com>
2015-10-20 01:05:25 -0400

    Update up.sh
    - Need to change over to PvPGN's build folder
    - Back to GCC (policy reasons :p)

--- Oak/up.sh
+++ Oak/up.sh
@@ -16,13 +16,14 @@ sync
 rm -R '/home/espionage724/build'
 mkdir '/home/espionage724/build'
 cd '/home/espionage724/build'
-CC='distcc clang' CXX='distcc clang++' cmake '/home/espionage724/trinitycore' -DTOOLS=1 -DCMAKE_INSTALL_PREFIX='/home/espionage724/run' -DWITH_WARNINGS=0 -DWITH_COREDEBUG=0 -DUSE_COREPCH=0 -DUSE_SCRIPTPCH=0 -DCMAKE_CXX_FLAGS='-O2 -pipe -march=amdfam10' -DCMAKE_C_FLAGS='-O2 -pipe -march=amdfam10' && sync
+CC='distcc gcc' CXX='distcc g++' cmake '/home/espionage724/trinitycore' -DTOOLS=1 -DCMAKE_INSTALL_PREFIX='/home/espionage724/run' -DWITH_WARNINGS=0 -DWITH_COREDEBUG=0 -DUSE_COREPCH=0 -DUSE_SCRIPTPCH=0 -DCMAKE_CXX_FLAGS='-O2 -pipe -march=amdfam10' -DCMAKE_C_FLAGS='-O2 -pipe -march=amdfam10' && sync
 DISTCC_HOSTS='192.168.1.150/8 localhost/1' make -j9 && sync
 
 # Recompile PvPGN
 rm -R '/home/espionage724/build-pvpgn'
 mkdir '/home/espionage724/build-pvpgn'
-CC='distcc clang' CXX='distcc clang++' cmake '/home/espionage724/pvpgn' -DCMAKE_INSTALL_PREFIX='/home/espionage724/run-pvpgn' -DWITH_MYSQL=1 -DWITH_LUA=0 -DWITH_WIN32_GUI=0 -DCMAKE_CXX_FLAGS='-O2 -pipe -march=amdfam10' -DCMAKE_C_FLAGS='-O2 -pipe -march=amdfam10' && sync
+cd '/home/espionage724/build-pvpgn'
+CC='distcc gcc' CXX='distcc g++' cmake '/home/espionage724/pvpgn' -DCMAKE_INSTALL_PREFIX='/home/espionage724/run-pvpgn' -DWITH_MYSQL=1 -DWITH_LUA=0 -DWITH_WIN32_GUI=0 -DCMAKE_CXX_FLAGS='-O2 -pipe -march=amdfam10' -DCMAKE_C_FLAGS='-O2 -pipe -march=amdfam10' && sync
 DISTCC_HOSTS='192.168.1.150/8 localhost/1' make -j9 && sync
 
 # Write changes to disk
