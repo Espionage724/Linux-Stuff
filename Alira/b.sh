5496d981474dd8d5a14d98bdeef6c6567d121329
Sean Rhone <espionage724@tutanota.com>
2015-10-23 18:57:26 -0400

    Update b.sh

--- Alira/b.sh
+++ Alira/b.sh
@@ -9,6 +9,7 @@ mysqldump -u dump -px joomla > '/home/espionage724/dumps/joomla.sql'
 mysqldump -u dump -px mybb > '/home/espionage724/dumps/mybb.sql'
 mysqldump -u dump -px wordpress > '/home/espionage724/dumps/wordpress.sql'
 mysqldump -u dump -px pvpgn > '/home/espionage724/dumps/pvpgn.sql'
+mysqldump -u dump -px murmur > '/home/espionage724/dumps/murmur.sql'
 
 # Archive databases
 zip -r backups/backup-$(date +%Y-%m-%d).zip dumps/*.sql
