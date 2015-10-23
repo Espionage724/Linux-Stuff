88f67799d9ec504b720a1c25bb4bad03848e9df5
Sean Rhone <espionage724@tutanota.com>
2015-10-23 10:03:14 -0400

    Update up.sh

--- Oak/up.sh
+++ Oak/up.sh
@@ -17,14 +17,14 @@ rm -R '/home/espionage724/build'
 mkdir '/home/espionage724/build'
 cd '/home/espionage724/build'
 CC='distcc gcc' CXX='distcc g++' cmake '/home/espionage724/trinitycore' -DTOOLS=1 -DCMAKE_INSTALL_PREFIX='/home/espionage724/run' -DWITH_WARNINGS=0 -DWITH_COREDEBUG=0 -DUSE_COREPCH=0 -DUSE_SCRIPTPCH=0 -DCMAKE_CXX_FLAGS='-O2 -pipe -march=amdfam10' -DCMAKE_C_FLAGS='-O2 -pipe -march=amdfam10' && sync
-DISTCC_HOSTS='192.168.1.150/8 localhost/1' make -j9 && sync
+DISTCC_HOSTS='192.168.1.150/8 localhost/2' make -j10 && sync
 
 # Recompile PvPGN
 rm -R '/home/espionage724/build-pvpgn'
 mkdir '/home/espionage724/build-pvpgn'
 cd '/home/espionage724/build-pvpgn'
 CC='distcc gcc' CXX='distcc g++' cmake '/home/espionage724/pvpgn' -DCMAKE_INSTALL_PREFIX='/home/espionage724/run-pvpgn' -DWITH_MYSQL=1 -DWITH_LUA=0 -DWITH_WIN32_GUI=0 -DCMAKE_CXX_FLAGS='-O2 -pipe -march=amdfam10' -DCMAKE_C_FLAGS='-O2 -pipe -march=amdfam10' && sync
-DISTCC_HOSTS='192.168.1.150/8 localhost/1' make -j9 && sync
+DISTCC_HOSTS='192.168.1.150/8 localhost/2' make -j10 && sync
 
 # Write changes to disk
 sync
