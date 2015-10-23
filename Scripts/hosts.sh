c5ea8fdfbc2106d3ac68aaca4280997ab6ed880c
Sean Rhone <espionage724@tutanota.com>
2015-10-23 02:37:52 -0400

    Update hosts.sh

--- Scripts/hosts.sh
+++ Scripts/hosts.sh
@@ -2,7 +2,6 @@ cd ~
 wget https://github.com/StevenBlack/hosts/archive/master.zip
 unzip 'master.zip'
 cd 'hosts-master'
-wget https://raw.githubusercontent.com/mjl152/hosts/master/updateHostsFile.py
 chmod +x 'updateHostsFile.py'
 python 'updateHostsFile.py'
 sudo cp 'hosts' '/etc/hosts'
