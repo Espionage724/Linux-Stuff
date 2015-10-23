a0f6933d034fdcc50793c1a664ac2cba82134466
Sean Rhone <espionage724@tutanota.com>
2015-10-23 07:22:10 -0400

    Create b.sh

---
+++ Alira/b.sh
@@ -0,0 +1,16 @@
+# Go home
+cd '/home/espionage724'
+
+# Backup databases
+mysqldump -u dump -px auth > '/home/espionage724/dumps/auth.sql'
+mysqldump -u dump -px characters > '/home/espionage724/dumps/characters.sql'
+mysqldump -u dump -px gnusocial > '/home/espionage724/dumps/gnusocial.sql'
+mysqldump -u dump -px joomla > '/home/espionage724/dumps/joomla.sql'
+mysqldump -u dump -px mybb > '/home/espionage724/dumps/mybb.sql'
+mysqldump -u dump -px wordpress > '/home/espionage724/dumps/wordpress.sql'
+
+# Archive databases
+zip -r backups/backup-$(date +%Y-%m-%d).zip dumps/*.sql
+
+# Finish up
+sync
